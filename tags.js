d4c0ea6199c3297e2cafd66d67c5d144a6ad8ef2
nvcken <nvcken@gmail.com>
2017-01-18 19:35:58 +0700

    update getTags

--- tags.js
+++ tags.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function getTags(text) {
-
+	var regexp = /\B\#\w\w+\b/g
+    var result = text.match(regexp);
+    var final = [];
+    if (result) {
+    	for(var i=0; i < result.length; i++){
+    		if( result[i] == result[i].toLowerCase() && 2 < result[i].length < 121 ){
+    			final.push(result[i]);
+    		}
+    	}
+        console.log(result);
+        return final;
+    } else {
+        return [];
+    }
 }
+
+console.log(getTags("I hate when people smoke e-cigarettes indoors. #annoying #rude #whygodwhy"));
 
 module.exports = {
   getTags: getTags
